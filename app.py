78728c4e53c18d740c821e8c5c08a9ba398c5f51
Seelim Valavuo <hello@seelim.dev>
2026-09-15 14:40:24 +0300

    feat: add new game form

--- app.py
+++ app.py
@@ -13,6 +13,26 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_game")
+def new_game():
+    return render_template("new_game.html")
+
+@app.route("/create_game", methods=["POST"])
+def create_game():
+    title = request.form["title"]
+    description = request.form["description"]
+    year = request.form["year"]
+    min_player_count = request.form["min_player_count"]
+    max_player_count = request.form["max_player_count"]
+    playtime = request.form["playtime"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO games (title, description, year, min_player_count, max_player_count, playtime, user_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?)"""
+    db.execute(sql, [title, description, year, min_player_count, max_player_count, playtime, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -43,16 +63,19 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
 
-    sql = "SELECT password_hash FROM users WHERE username = ?"
-    query = db.query(sql, [username])
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    query = db.query(sql, [username])[0]
+    user_id = query["id"]
 
-    if not query or not check_password_hash(query[0][0], password):
+    if not query or not check_password_hash(query[1], password):
         return "ERROR: wrong username or password"
 
+    session["user_id"] = user_id
     session["username"] = username
     return redirect("/")
 
 @app.route("/logout")
 def logout():
     session.pop("username", None)
+    session.pop("user_id", None)
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,14 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    year INTEGER,
+    min_player_count INTEGER,
+    max_player_count INTEGER,
+    playtime INTEGER,
+    user_id INTEGER REFERENCES users
+);
